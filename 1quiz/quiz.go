0c428ab9bad4f5c8b22f40c7130c384de13237d7
Gagan Mishra <simonsimple305@gmail.com>
2025-12-24 16:57:06 +0530

    q1 p2 - added timer

--- 1quiz/quiz.go
+++ 1quiz/quiz.go
@@ -5,9 +5,13 @@ import (
 	"os"
 	"encoding/csv"
 	"time"
+	"flag"
 )
 
 func main() {
+	questionTime := flag.Int("qtime", 5, "time limit per question in seconds")
+    flag.Parse()
+
 	file, err := os.Open("problems.csv")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
@@ -25,11 +29,22 @@ func main() {
 	correct := 0
 	for i,record := range records {
 		fmt.Printf("Question %v: %v \n", i, record[0])
-		var ans string
-		fmt.Scanf("%s", &ans)
 
-		if ans == record[1] {
-			correct ++
+		// adding a timer
+		answerCh := make(chan string, 1)
+		go func() {
+			var ans string
+			fmt.Scanf("%s", &ans)
+			answerCh <- ans
+		}()
+
+		select {
+		case ans := <-answerCh:
+			if ans == record[1]{
+				correct++
+			}
+		case <-time.After(time.Duration(*questionTime) * time.Second):
+			fmt.Println("Time's up for this question")
 		}
 	}
 
